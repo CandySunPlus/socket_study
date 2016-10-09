50622ff5f9d99179436dbf8ff78e2e3cca040899
Fengming Sun <sunfengming@meituan.com>
2016-10-09 16:54:42 +0800

    finish server

--- server.c
+++ server.c
@@ -3,10 +3,11 @@
 int main(int argc, char *argv[])
 {
     struct sockaddr_in server_addr, client_addr;
-    int listen_fd, conn_fd;
+    int listen_fd, conn_fd, opt = 1;
     pid_t child_pid;
     socklen_t client_len;
 
+    // 1) init socket
     if ((listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) < 0) {
         perror("socket error");
         exit(1);
@@ -17,16 +18,25 @@ int main(int argc, char *argv[])
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(PORT);
 
+    // 2) set socket reuse address
+    if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("socket set error");
+        exit(1);
+    }
+
+    // 3) bind socket to address and port
     if (bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind error");
         exit(1);
     }
 
+    // 4) listen
     if (listen(listen_fd, LISTENQ) < 0) {
         perror("listen error");
         exit(1);
     }
 
+    // 5) receive client request
     for ( ; ; ) {
         client_len = sizeof(client_addr);
         if ((conn_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len)) < 0) {
